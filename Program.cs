ca90a2d06aef5c4f529198442b6fda205bedfeac
MohamedSantos <mohamedprofissional22@gmail.com>
2021-05-18 11:35:28 -0300

    first

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace CsharpCEP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o CEP: ");
+            string cep = Console.ReadLine();
+            var remoteUrl = WebRequest.CreateHttp(string.Format("https://buscacepinter.correios.com.br/app/endereco/carrega-cep-endereco.php?pagina=%2Fapp%2Fendereco%2Findex.php&cepaux=&mensagem_alerta=&endereco={0}&tipoCEP=ALL", cep));
+            using(var response = remoteUrl.GetResponse())
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                object result = reader.ReadToEnd();
+                var dataList = JsonConvert.DeserializeObject<Rootobject>(result.ToString());
+                var dados = dataList.dados[0];
+                Console.WriteLine("Endereço: " + dados.logradouroDNEC + "\nCidade: " + dados.localidade + "\nUF: "+ dados.uf);
+            }
+         
+        }
+    }
+}
---
+++ Rootobject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpCEP
+{
+    public class Rootobject
+    {
+        public bool erro { get; set; }
+        public string mensagem { get; set; }
+        public int total { get; set; }
+        public Dado[] dados { get; set; }
+    }
+
+    public class Dado
+    {
+        public string uf { get; set; }
+        public string localidade { get; set; }
+        public string locNu { get; set; }
+        public string localidadeSubordinada { get; set; }
+        public string logradouroDNEC { get; set; }
+        public string logradouroTextoAdicional { get; set; }
+        public string logradouroTexto { get; set; }
+        public string bairro { get; set; }
+        public string baiNu { get; set; }
+        public string nomeUnidade { get; set; }
+        public string cep { get; set; }
+        public string tipoCep { get; set; }
+        public string numeroLocalidade { get; set; }
+        public string situacao { get; set; }
+    }
+
+}
